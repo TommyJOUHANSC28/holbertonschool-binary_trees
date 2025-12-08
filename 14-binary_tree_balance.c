e9ecdeaa92125756991503e031b0c370ed139cdd
Tommy JOUHANS <12119@holbertonstudents.com>
2025-12-08 22:25:31 +0000

    binary tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,12 +6,12 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-size_t lheight = 1, rheight = 1;
+size_t lheight, rheight;
 if (!tree)
 return (0);
-lheight = tree->left ? -1 + (int) binary_tree_height(tree->left) : 0;
-rheight = tree->right ? -1 + (int) binary_tree_height(tree->right) : 0;
-return (lheight > rheight ? lheight : rheight);
+lheight = tree->left ? (int) binary_tree_height(tree->left) : -1;
+rheight = tree->right ? (int) binary_tree_height(tree->right) : -1;
+return (lheight - rheight);
 }
 /**
 * binary_tree_height -  function that measures the height of a binary tree
