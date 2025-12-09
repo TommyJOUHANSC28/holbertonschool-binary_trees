da74c70849cb8434e0f065df82fa4925d2a32903
Tommy JOUHANS <12119@holbertonstudents.com>
2025-12-09 10:24:14 +0000

    binary tree inset right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,8 +8,10 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 binary_tree_t *new;
-new = binary_tree_node(parent, value);
 if (parent == NULL)
+return (NULL);
+new = binary_tree_node(parent, value);
+if (new == NULL)
 {
 return (NULL);
 }
