24d8eace14fda83b6616e5ce925c853a4de988c8
Tommy JOUHANS <12119@holbertonstudents.com>
2025-12-09 13:29:18 +0000

    binary tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,9 +9,9 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-if (tree)
+if (tree == NULL)
 return (0);
-if (!tree->left  && !tree->right)
+if (!tree->right && !tree->left)
 return (1);
 if (tree->right && tree->left)
 return (binary_tree_is_full(tree->left) &&
